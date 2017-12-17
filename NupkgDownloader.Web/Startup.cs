9c85fcc87cb4562eb6d184fc018e987b9f77f1f9
FormatDeng <goscan@live.cn>
2017-12-18 01:25:02 +0800

    add default routes;

--- NupkgDownloader.Web/Startup.cs
+++ NupkgDownloader.Web/Startup.cs
@@ -107,9 +107,12 @@ namespace NupkgDownloader.Web
                 //    domainTemplate: "{xx}.newmsil.cn",
                 //    routeTemplate: "{controller=Home}/{action=Index}/{id?}"
                 //        );
-
                 routes.MapRoute(
                     name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default1",
                     template: "{controller=Home}/{action=Index}/{id?}",
                     defaults: new RouteValueDictionary
                     {
