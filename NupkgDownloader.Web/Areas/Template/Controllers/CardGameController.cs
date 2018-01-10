a071e4120576e1b134face7b6ebb76849f841ae3
FormatDeng <goscan@live.cn>
2018-01-11 01:55:24 +0800

    Add game controller;remove independent game folder;

---
+++ NupkgDownloader.Web/Areas/Template/Controllers/CardGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NupkgDownloader.Web.Controllers
+{
+    [Area("Template")]
+    public class CardGameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
